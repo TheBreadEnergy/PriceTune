3c55c7539cb32ebbc974f82c9321e406c1a852a3
TheBreadEnergy <thebreadenergy@gmail.com>
2024-10-17 23:14:06 +0300

    fix nginx.conf

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
-# Замена переменной ${SERVER_NAME} в nginx.conf
-sed -i "s|\${SERVER_NAME}|${SERVER_NAME}|g" /etc/nginx/nginx.conf
+# Проверяем наличие сертификатов Let's Encrypt
+if [ -f "/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem" ]; then
+    SSL_CERTIFICATE="/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem"
+    SSL_CERTIFICATE_KEY="/etc/letsencrypt/live/${SERVER_NAME}/privkey.pem"
+else
+    SSL_CERTIFICATE="/etc/nginx/ssl/server.crt"
+    SSL_CERTIFICATE_KEY="/etc/nginx/ssl/server.key"
+fi
 
-# Запуск Nginx с самоподписанным сертификатом
+# Подставляем значения в шаблон конфигурации
+envsubst '${SERVER_NAME} ${SSL_CERTIFICATE} ${SSL_CERTIFICATE_KEY}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+
+# Запуск Nginx
 nginx
 
-# Запуск Certbot для получения реального сертификата
-certbot certonly --webroot -w /var/lib/letsencrypt --non-interactive --agree-tos --email ${CERTBOT_EMAIL} -d ${SERVER_NAME}
+# Попытка получить сертификат с помощью Certbot, если его еще нет
+if [ ! -f "/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem" ]; then
+    certbot certonly --webroot -w /var/lib/letsencrypt --non-interactive --agree-tos --email ${CERTBOT_EMAIL} -d ${SERVER_NAME}
 
-# Перезагрузка Nginx после получения сертификатов
-nginx -s reload
+    # Если сертификат успешно получен, обновляем конфигурацию и перезагружаем Nginx
+    if [ -f "/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem" ]; then
+        echo "Real certificate obtained, updating Nginx configuration..."
+        SSL_CERTIFICATE="/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem"
+        SSL_CERTIFICATE_KEY="/etc/letsencrypt/live/${SERVER_NAME}/privkey.pem"
+        envsubst '${SERVER_NAME} ${SSL_CERTIFICATE} ${SSL_CERTIFICATE_KEY}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+        nginx -s reload
+    fi
+fi
 
 # Бесконечный цикл для автоматического обновления сертификатов и перезагрузки Nginx
 while :; do
